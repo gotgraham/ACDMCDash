14b208258845158d7c2a8ec3f2b6dcdcda3755d0
Jacob Graham <jake@shortstacklab.com>
2019-09-02 09:22:13 -0700

    Initial pass

---
+++ app/src/main/java/com/empire/ac_dmc_dashboard/SerialListener.java
@@ -0,0 +1,8 @@
+package com.empire.ac_dmc_dashboard;
+
+interface SerialListener {
+    void onSerialConnect      ();
+    void onSerialConnectError (Exception e);
+    void onSerialRead         (byte[] data);
+    void onSerialIoError      (Exception e);
+}
